6795081358d5e0b356dfac91e74fe185c70affe3
Muhammad Tabish <132977467+Raja-tabish@users.noreply.github.com>
2024-03-07 16:48:12 +0500

    Add files via upload

---
+++ Assignment 19/app.js
@@ -0,0 +1,42 @@
+// ..............Chapter 14 (USER INPUT & CONDITIONALSTATEMENT)...................
+
+
+// Qno1
+// var cityName = prompt("Enter the city name:");
+// if (cityName.toLowerCase() === "karachi") {
+//     document.write("Welcome to the city of lights!");
+// } else {
+//     document.write("Welcome to " + cityName);
+// };
+
+// Qno2
+// var gender=prompt("Enter your Gender:");
+// if(gender==="male"){
+//     document.write("Good Morning Sir");
+// }else if (gender==="female"){
+//     document.write("Good Morning Ma'am")
+// }else{
+//     document.write("please choose between male & female or in lowercase")
+// }
+
+// Qno3
+// var signalColor = prompt("Enter the color of the traffic signal: ");
+// if ( signalColor=== "red"){
+//     document.write("Must Stop")
+// }else if (signalColor === "yellow"){
+//     document.write("Ready to move")
+// }else if (signalColor=== "green"){
+//     document.write("Move now")
+// }else{
+//     document.write("Invalid signal color. Please enter 'Red', 'Yellow', or 'Green'.")
+// }
+
+// Qno4
+var remainingFuel = prompt("Enter the remaining fuel in your car (in litres):");
+// var remainingFuel = parseFloat(remainingFuel);
+// if (remainingFuel < 0.25) {
+//     document.write("Please refill the fuel in your car");
+// } else if {
+//     document.write("You have enough fuel. Safe travels!");
+// }
+
